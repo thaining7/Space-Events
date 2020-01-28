99bbe30da404043a725e6aa94697d6b2c9826b87
Trevor <thaining@protonmail.com>
2020-01-28 11:02:16 -0600

    trying dotenv heroku workaround

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,7 @@
 // const dotenv = require('dotenv');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+};
 const db = require("../models");
 const request = require('request');
 const axios = require("axios");
